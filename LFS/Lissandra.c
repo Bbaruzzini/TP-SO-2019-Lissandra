4346c01f2e75dd15fede79233a12da65be1ad5d2
DeniseDee <dassodenise@hotmail.com>
2019-04-26 10:32:54 -0300

    Creacion Config Lissandra y mas pruebas
    Signed-off-by: Bbaruzzini <bren_baru@live.com.ar>

--- LFS/Lissandra.c
+++ LFS/Lissandra.c
@@ -1,6 +1,17 @@
-//
-
-//
+#include "Appender.h"
+#include "AppenderConsole.h"
+#include "AppenderFile.h"
+#include "Console.h"
+#include "CLIHandlers.h"
+#include "EventDispatcher.h"
+#include "FileWatcher.h"
+#include "Logger.h"
+#include <libcommons/config.h>
+#include <libcommons/string.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <signal.h>
 
 #include "Lissandra.h"
 
