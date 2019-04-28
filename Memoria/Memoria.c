112893f16bf6cd054a1671a154e1c02fdc967b2e
Angel Luis Eduardo Gomez <angelluisgo@gmail.com>
2019-04-28 17:13:33 -0300

    Para responder los comandos.
    Modificacion del archivo de configuracion

--- Memoria/Memoria.c
+++ Memoria/Memoria.c
@@ -1,3 +1,29 @@
+#include "Appender.h"
+#include "AppenderConsole.h"
+#include "AppenderFile.h"
+#include "CLIHandlers.h"
+#include "Config.h"
+#include "Console.h"
+#include "EventDispatcher.h"
+#include "FileWatcher.h"
+#include "Logger.h"
+#include <libcommons/config.h>
+#include <libcommons/string.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+CLICommand CLICommands[] =
+{
+    { "SELECT",   HandleSelect   },
+    { "INSERT",   HandleInsert   },
+    { "CREATE",   HandleCreate   },
+    { "DESCRIBE", HandleDescribe },
+    { "DROP",     HandleDrop     },
+    { "JOURNAL",  HandleJournal  },
+    { NULL,       NULL           }
+};
 
 int main(void)
 {
