67a8d2caf57050c601305801c11157fff0a9db94
Eliseo Derghazarian <elider9221@gmail.com>
2019-04-28 17:18:02 -0300

    cli

---
+++ Memoria/CLIHandlers.h
@@ -0,0 +1,11 @@
+#ifndef Memoria_CLIHandlers_h__
+#define Memoria_CLIHandlers_h__
+
+void HandleSelect(char const* args);
+void HandleInsert(char const* args);
+void HandleCreate(char const* args);
+void HandleDescribe(char const* args);
+void HandleDrop(char const* args);
+void HandleJournal(char const* args);
+
+#endif //Memoria_CLIHandlers_h__
