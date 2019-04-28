09c6c722cd50277cb3a5d9984a680bbe04504a58
Eliseo Derghazarian <elider9221@gmail.com>
2019-04-28 17:52:04 -0300

    Memoria CLIHandlers.c

---
+++ Memoria/CLIHandlers.c
@@ -0,0 +1,33 @@
+#include "CLIHandlers.h"
+#include <stddef.h>
+
+void HandleSelect(char const* args)
+{
+    (void) args;
+}
+
+void HandleInsert(char const* args)
+{
+    (void) args;
+}
+
+void HandleCreate(char const* args)
+{
+    (void) args;
+}
+
+void HandleDescribe(char const* args)
+{
+    (void) args;
+}
+
+void HandleDrop(char const* args)
+{
+    (void) args;
+}
+
+void HandleJournal(char const* args)
+{
+    (void) args;
+}
+
