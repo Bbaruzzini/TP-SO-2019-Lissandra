519f57e0d3a0d7c8d8daecd6bab4a4acda6c8cb8
ariel- <ariel-@users.noreply.github.com>
2019-05-24 11:45:19 -0300

    Shared: Agregada documentacion de flags de logger

--- Shared/Logging/LogCommon.h
+++ Shared/Logging/LogCommon.h
@@ -17,10 +17,17 @@ typedef enum
 
 typedef enum
 {
+    /* opciones generales */
     APPENDER_FLAGS_NONE             = 0x00,
+    // prefijar fecha y hora en cada log (precision milisegundos)
     APPENDER_FLAGS_PREFIX_TIMESTAMP = 0x01,
+    // prefijar nivel de log en cada entrada de log (INFO/ERROR/etc.)
     APPENDER_FLAGS_PREFIX_LOGLEVEL  = 0x02,
+
+    /* opciones para AppenderFile (archivos) */
+    // agregar fecha y hora de creacion luego del nombre de archivo
     APPENDER_FLAGS_USE_TIMESTAMP    = 0x04,
+    // si el archivo existe se genera un backup y se crea uno nuevo (solo si el modo de apertura es "w")
     APPENDER_FLAGS_MAKE_FILE_BACKUP = 0x08
 } AppenderFlags;
 
