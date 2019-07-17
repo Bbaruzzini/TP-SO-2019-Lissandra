a158f64c04fa6ae353ddc4e61e353a5d45b1b545
ariel- <ariel-@users.noreply.github.com>
2019-07-17 19:42:57 -0300

    Actualizo valores scripts

--- deploy/adv_kernel/kernel.sh
+++ deploy/adv_kernel/kernel.sh
@@ -3,7 +3,7 @@ IP3=IP_MEMORIA3
 
 echo IP_MEMORIA=$IP3 > kernel.conf
 echo PUERTO_MEMORIA=8001 >> kernel.conf
-echo QUANTUM=3 >> kernel.conf
+echo QUANTUM=1 >> kernel.conf
 echo MULTIPROCESAMIENTO=1 >> kernel.conf
 echo METADATA_REFRESH=15000 >> kernel.conf
 echo SLEEP_EJECUCION=1000 >> kernel.conf
--- deploy/adv_stress/kernel.sh
+++ deploy/adv_stress/kernel.sh
@@ -3,8 +3,8 @@ IP5=IP_MEMORIA5
 
 echo IP_MEMORIA=$IP5 > kernel.conf
 echo PUERTO_MEMORIA=8002 >> kernel.conf
-echo QUANTUM=3 >> kernel.conf
-echo MULTIPROCESAMIENTO=1 >> kernel.conf
+echo QUANTUM=1 >> kernel.conf
+echo MULTIPROCESAMIENTO=3 >> kernel.conf
 echo METADATA_REFRESH=15000 >> kernel.conf
 echo SLEEP_EJECUCION=10 >> kernel.conf
 
